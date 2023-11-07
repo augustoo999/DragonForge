21a9531a502219665417819d2e2cec8a4ad1d4a5
Oguibecker <132964768+Oguibecker@users.noreply.github.com>
2023-11-06 21:58:06 -0300

    KillerTrigger

---
+++ Assets/Scripts/KillerTrigger.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KillerTrigger : MonoBehaviour
+{
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<Player>(out var player))
+        {
+            SceneManager.LoadScene(2);
+        }
+    }
+}
