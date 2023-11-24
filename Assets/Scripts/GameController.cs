531c848d60c0c99fb558d96aa6588ceea25a9e6e
Oguibecker <132964768+Oguibecker@users.noreply.github.com>
2023-11-24 15:42:04 -0300

    Implementação das plataformas

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,17 +4,32 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    public string[] PlayerNames;
+    public GameObject[] conjuntosDePlataformas;
+    public float distanciaEntrePlataformas = 1f;
 
-    public ScriptableObject sc;
-
-    public GameObject ene;
-   
     void Start()
     {
-        Instantiate(ene);
-        Debug.Log(sc.name);
-        //ene.GetComponent<MovimentoInimigo>().Speed = sc.movespeed;
+        SortearPlataformas();
     }
 
+    void SortearPlataformas()
+    {
+        Vector3 posicaoInicial = Vector3.zero;
+
+        for (int i = 0; i < 5; i++)
+        {
+            int indiceSorteado = Random.Range(0, conjuntosDePlataformas.Length);
+            GameObject conjuntoSorteado = conjuntosDePlataformas[indiceSorteado];
+
+            // Use o conjuntoSorteado conforme necessário
+            GameObject plataforma = Instantiate(conjuntoSorteado, posicaoInicial, Quaternion.identity);
+
+            // Obter a largura do conjunto para garantir a próxima posição
+            Renderer rend = plataforma.GetComponentInChildren<Renderer>();
+            float larguraConjunto = rend != null ? rend.bounds.size.x : 0f;
+
+            // Atualizar a posição inicial para a próxima plataforma
+            posicaoInicial.x += larguraConjunto + distanciaEntrePlataformas;
+        }
+    }
 }
