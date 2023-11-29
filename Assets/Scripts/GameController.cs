f3d6a0760121121753243b51f255ce9bf4b49b08
Oguibecker <132964768+Oguibecker@users.noreply.github.com>
2023-11-29 01:56:42 -0300

    Fase creation

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,35 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class GameController : MonoBehaviour
 {
-    public GameObject[] conjuntosDePlataformas;
-    public float distanciaEntrePlataformas = 1f;
-
-    void Start()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        SortearPlataformas();
-    }
-
-    void SortearPlataformas()
-    {
-        Vector3 posicaoInicial = Vector3.zero;
-
-        for (int i = 0; i < 5; i++)
+        if (collision.TryGetComponent<Player>(out var player))
         {
-            int indiceSorteado = Random.Range(0, conjuntosDePlataformas.Length);
-            GameObject conjuntoSorteado = conjuntosDePlataformas[indiceSorteado];
-
-            // Use o conjuntoSorteado conforme necessário
-            GameObject plataforma = Instantiate(conjuntoSorteado, posicaoInicial, Quaternion.identity);
-
-            // Obter a largura do conjunto para garantir a próxima posição
-            Renderer rend = plataforma.GetComponentInChildren<Renderer>();
-            float larguraConjunto = rend != null ? rend.bounds.size.x : 0f;
-
-            // Atualizar a posição inicial para a próxima plataforma
-            posicaoInicial.x += larguraConjunto + distanciaEntrePlataformas;
+            SceneManager.LoadScene(1);
         }
     }
 }
